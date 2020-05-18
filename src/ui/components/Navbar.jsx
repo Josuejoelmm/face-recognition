7cd613d6252dbf716c0f645b52765bd3bc907fab
josue <josuejoelmm@gmail.com>
2020-05-18 17:22:26 -0300

    creating navbar component

---
+++ src/ui/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Navbar() {
+    return(
+        <header>
+            <div>
+                <div>
+                    <a href="#" >Sign in</a>
+                </div>
+            </div>
+        </header>
+    );
+};
